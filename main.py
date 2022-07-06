09c28fee40a349c1dd82c8ec25940d00bdf3ba56
Nato <1900011764@pku.edu.cn>
2022-07-06 17:08:08 +0800

    main programme

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import torch.cuda
 
 from modules.Predictor import DS_GCNPredictor, DS_CycleGCNPRedictor, SimpleLayerGCNPredictor
 from utils import PredictAlloRes
-from configs import args_main
+from configs import use_arg_main
 
 if __name__ == "__main__":
+    args_main = use_arg_main()
     print(args_main)
     device = "cuda" if torch.cuda.is_available() else "cpu"
     PModel = None
@@ -17,9 +18,13 @@ if __name__ == "__main__":
     else:
         raise ValueError("No such predict model.")
 
+    out_file = None
     if args_main.pdb_id:
-        PredictAlloRes(Predictor_model=PModel, Predictor_params_file=args_main.pf,
-                       mode=args_main.mode, PDB_ID=args_main.pdb_id)
+        out_file = PredictAlloRes(Predictor_model=PModel, Predictor_params_file=args_main.pf, PDB_ID=args_main.pdb_id,
+                                  visualize=bool(args_main.v), vis_mode=args_main.vis_mode)
     else:
-        PredictAlloRes(Predictor_model=PModel, Predictor_params_file=args_main.pf,
-                       mode=args_main.mode, pdb_file_path=args_main.f)
+        out_file = PredictAlloRes(Predictor_model=PModel, Predictor_params_file=args_main.pf, pdb_file_path=args_main.f,
+                                  visualize=bool(args_main.v), vis_mode=args_main.vis_mode)
+
+    print("Predicted info of allosteric sites has been writen.")
+    print("Please check file path {}.".format(out_file))
